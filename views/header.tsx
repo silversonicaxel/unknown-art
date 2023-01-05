0b11f14134db253c93921fd91ac77998f5d9ed3b
Alessandro Rabitti <axel83.beta@gmail.com>
2023-01-05 20:15:54 +0100

    Added favicons

--- views/header.tsx
+++ views/header.tsx
@@ -18,6 +18,24 @@ const Header = (props: HeaderProps) => {
         href='https://fonts.googleapis.com/css?family=Space Mono&display=optional'
         rel='stylesheet'
       />
+      <link
+        rel='apple-touch-icon'
+        sizes='180x180'
+        href='/apple-touch-icon.png'
+      />
+      <link
+        rel='icon'
+        type='image/png'
+        sizes='32x32'
+        href='/favicon-32x32.png'
+      />
+      <link
+        rel='icon'
+        type='image/png'
+        sizes='16x16'
+        href='/favicon-16x16.png'
+      />
+      <link rel='manifest' href='/site.webmanifest' />
     </Head>
   )
 }
