c0c1f92c54dd257b44c695e6c58e6a17dce12c43
Alessandro Rabitti <axel83.beta@gmail.com>
2023-01-05 23:10:42 +0100

    Improved the navigational menu

--- views/menu/menu.tsx
+++ views/menu/menu.tsx
@@ -1,19 +1,34 @@
 import styles from './menu.module.css'
 import { memo } from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const Menu = () => {
   return (
-    <nav className={styles.uanav}>
-      <ul>
-        <li>
-          <Link href='/'>home</Link>
-        </li>
-        <li>
-          <Link href='places'>places</Link>
-        </li>
-      </ul>
-    </nav>
+    <header className={styles.uaheader}>
+      <Link href='/'>
+        <Image
+          src='/logo.png'
+          alt='ACME Inc.'
+          width={80}
+          height={80}
+          className={styles.ualogo}
+          role='navigation'
+          aria-label='main logo'
+        />
+      </Link>
+
+      <nav className={styles.uanav}>
+        <ul aria-labelledby='menu'>
+          <li>
+            <Link href='/'>home</Link>
+          </li>
+          <li>
+            <Link href='/places'>places</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
