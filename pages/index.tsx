85a22441a31fd0e535826d3c7ac28017f064a2cb
Alessandro Rabitti <axel83.beta@gmail.com>
2023-04-01 10:25:46 +0200

    Added hero image to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
+import HeroImage from '../views/hero-image/hero-image'
+
 const Home = () => {
   return (
     <>
       <h1>unknown art</h1>
+
+      <HeroImage />
     </>
   )
 }
---
+++ views/hero-image/hero-image.tsx
@@ -0,0 +1,19 @@
+import React, { useRef, useEffect } from 'react'
+import styles from './hero-image.module.css'
+import Image from 'next/image'
+
+const HeroImage = () => {
+  return (
+    <div className={styles.uaheroimage}>
+      <Image
+        src='/hero-image.png'
+        alt='hero image unknown art'
+        role='presentation'
+        aria-label='hero image unknown art'
+        fill
+      />
+    </div>
+  )
+}
+
+export default HeroImage
