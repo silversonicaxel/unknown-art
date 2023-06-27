0950ad1d7a973d5b8bbfe27246de551f2ae86f7f
Alessandro Rabitti <axel83.beta@gmail.com>
2023-06-27 14:39:39 +0200

    Fixed onError in SafeImage component

--- views/safe-image/safe-image.tsx
+++ views/safe-image/safe-image.tsx
@@ -1,5 +1,5 @@
 import Image, { ImageLoader } from 'next/image'
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import type { FC } from 'react'
 
 type SafeImageProps = {
@@ -12,17 +12,13 @@ type SafeImageProps = {
 export const SafeImage: FC<SafeImageProps> = (props) => {
   const [hideImage, setHideImage] = useState(false)
 
+  const onError = useCallback(() => {
+    setHideImage(true)
+  }, [])
+
   if (hideImage) return null
 
-  return (
-    <Image
-      role='img'
-      {...props}
-      onError={() => {
-        setHideImage(true)
-      }}
-    />
-  )
+  return <Image role='img' {...props} onError={onError} />
 }
 
 SafeImage.displayName = 'SafeImage'
