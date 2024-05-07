1459135462d080aa4948166637e1c9dd27250d49
Alessandro Rabitti <axel83.beta@gmail.com>
2024-05-07 16:20:30 +0200

    Added api with query

--- src/api/place/index.ts
+++ src/api/place/index.ts
@@ -1,12 +1,20 @@
 import clientPromise from 'src/config/mongodb'
+import type { ApiQuery } from 'src/types/api'
 import type { Place } from 'src/types/place'
 
-export const getPlaces = async (): Promise<Place[]> => {
+export const getPlacesList = async (query: ApiQuery = {}): Promise<Place[]> => {
+  const { search, ...options } = query
+  const findFilter = search ? { name: { $regex: search, $options: 'si' } } : {}
+  const findOptions = {
+    limit: options.limit,
+    skip: (options.offset ?? 0) * (options.limit ?? 0)
+  }
+
   const mongoClient = await clientPromise
   const data = await mongoClient
     .db('ua-db')
     .collection('ua-places')
-    .find({})
+    .find(findFilter, findOptions)
     .collation({ locale: 'en' })
     .sort({ name: 1 })
     .toArray()
