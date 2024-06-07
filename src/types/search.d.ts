03a4c06cbbb9364f5d7ea27dc01cc12dcac71d3a
Alessandro Rabitti <axel83.beta@gmail.com>
2024-06-07 16:26:27 +0200

    Added search places with website flag

--- src/types/search.d.ts
+++ src/types/search.d.ts
@@ -2,10 +2,12 @@ export type SearchPlacesFormInput = {
   name?: string
   iso?: string
   city?: string
+  website?: string
 }
 
 export type SearchPlacesApi = {
   name?: { $regex: string, $options: string }
   iso?: string
   city?: { $regex: string, $options: string }
+  site?: { $exists: boolean }
 }
--- src/utils/isObjectNull.ts
+++ src/utils/isObjectNull.ts
@@ -1,6 +1,8 @@
 import type { EmptyObject } from 'type-fest'
 
 
-export const isObjectNull = (obj: Record<string, string | null | undefined> | EmptyObject) => {
-  return Object.values(obj).every(value =>  value === '' || value === null || value === undefined)
+export const isObjectNull = (
+  obj: Record<string, string | null | undefined> | EmptyObject
+) => {
+  return Object.values(obj).every(value => value === '' || value === null || value === undefined)
 }
