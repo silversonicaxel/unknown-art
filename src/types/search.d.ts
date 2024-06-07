90b654fe837aa72fa3206b72848c56326f0bb17f
Alessandro Rabitti <axel83.beta@gmail.com>
2024-06-07 15:43:33 +0200

    Added search places with city

--- src/types/search.d.ts
+++ src/types/search.d.ts
@@ -1,9 +1,11 @@
 export type SearchPlacesFormInput = {
   name?: string
   iso?: string
+  city?: string
 }
 
 export type SearchPlacesApi = {
   name?: { $regex: string, $options: string }
   iso?: string
+  city?: { $regex: string, $options: string }
 }
