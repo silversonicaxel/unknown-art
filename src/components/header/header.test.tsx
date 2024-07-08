6b20a8c191ba84b100725b41b78c8a7953adf7f7
Alessandro Rabitti <axel83.beta@gmail.com>
2024-07-08 11:23:41 +0200

    Added menu unit tests

--- src/components/header/header.test.tsx
+++ src/components/header/header.test.tsx
@@ -1,36 +1,32 @@
 import { render, screen } from '@testing-library/react'
-import { describe, expect, test, vi } from 'vitest'
+import { beforeAll, afterAll, describe, expect, test } from 'vitest'
+
+import {
+  viMockNextNavigation,
+  viMockReactI18next,
+  viMockStateReset
+} from '../../../vitest.setup.mjs'
+
 
 import { Header } from './header'
 
 
-vi.mock('next/navigation', () => {
-  return {
-    __esModule: true,
-    useParams: () => ({
-      locale: 'en'
-    })
-  }
-})
+describe('Components > Header', () => {
+  beforeAll(() => {
+    viMockNextNavigation()
+    viMockReactI18next()
+  })
 
-vi.mock('react-i18next', () => {
-  return {
-    __esModule: true,
-    useTranslation: () => ({
-      t: (translation: string) => translation,
-      i18n: {
-        resolvedLanguage: 'en',
-        changeLanguage: () => new Promise(() => {}),
-      }
-    }),
-    initReactI18next: {
-      type: '3rdParty',
-      init: () => {},
-    },
-  }
-})
+  afterAll(() => {
+    viMockStateReset()
+  })
 
-describe('Views > Header', () => {
+  test('display navigation element as logo', () => {
+    render(<Header />)
+
+    expect(screen.getByLabelText('common_logo.label')).toHaveProperty('role', 'navigation')
+  })
+
   test('display logo', () => {
     render(<Header />)
 
--- vitest.setup.mts
+++ vitest.setup.mts
@@ -1,6 +1,42 @@
 import { cleanup } from '@testing-library/react'
-import { afterEach } from 'vitest'
+import { vi, afterEach } from 'vitest'
 
+
+export const viMockNextNavigation = () => {
+  vi.mock('next/navigation', () => {
+    return {
+      __esModule: true,
+      useParams: () => ({
+        locale: 'en'
+      })
+    }
+  })
+}
+
+export const viMockReactI18next = () => {
+  vi.mock('react-i18next', () => {
+    return {
+      __esModule: true,
+      useTranslation: () => ({
+        t: (translation: string) => translation,
+        i18n: {
+          resolvedLanguage: 'en',
+          changeLanguage: () => new Promise(() => {}),
+        }
+      }),
+      initReactI18next: {
+        type: '3rdParty',
+        init: () => {},
+      },
+    }
+  })
+}
+
+export const viMockStateReset = () => {
+  vi.clearAllMocks()
+  vi.resetAllMocks()
+  vi.restoreAllMocks()
+}
 
 afterEach(() => {
   cleanup()
