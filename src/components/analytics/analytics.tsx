6ac1f47220c9771a10ff933b4a75fdd70cfe839b
Alessandro Rabitti <axel83.beta@gmail.com>
2024-08-23 11:14:10 +0200

    Added analytics

---
+++ src/components/analytics/analytics.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import Script from 'next/script'
+import { memo } from 'react'
+import type { FC } from 'react'
+
+
+export const Analytics: FC = memo(() => {
+  return (
+    <>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-ED84KW8XBQ" />
+      <Script id="analytics">
+        {
+          `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-ED84KW8XBQ');`
+        }
+      </Script>
+    </>
+  )
+})
+
+Analytics.displayName = 'Analytics'
