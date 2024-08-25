dfc77898f25b5e899db9fbb8b3b9924997d0f56a
Alessandro Rabitti <axel83.beta@gmail.com>
2024-08-25 13:26:33 +0200

    Added search results

---
+++ src/components/search-results/search-results.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import Link from 'next/link'
+import { useParams } from 'next/navigation'
+import type { FC } from 'react'
+
+import { Pagination } from '../pagination'
+
+import styles from './search-results.module.css'
+
+import { useTranslationClient } from 'src/helpers/hooks/useTranslationClient'
+import type { CountryCode } from 'src/types/country'
+import type { Place } from 'src/types/place'
+
+
+type SearchResultsProps = {
+  places: Place[]
+  totalPlaces: number,
+  countryCodes: CountryCode
+}
+
+export const SearchResults: FC<SearchResultsProps> = ({ places, totalPlaces, countryCodes }) => {
+  const params = useParams()
+  const locale = params.locale as string
+
+  const { t } = useTranslationClient({ locale, namespace: 'places' })
+
+  if (places.length > 0 && totalPlaces > 0) {
+    return (
+      <>
+        {places.map((place) => (
+          <div key={`place-${place.id}`} className={styles.uasearchresults__item}>
+            <Link href={`/${locale}/places/${place.id}`}>
+              <span>{place.name}</span>
+              &nbsp;
+              <span className={styles['uasearchresults__item-separator']}>~~</span>
+              &nbsp;
+              <span>
+                {place.city ? `${place.city} - ` : ``}
+                {countryCodes[place.iso]}
+              </span>
+            </Link>
+          </div>
+        ))}
+
+        <Pagination totalItems={totalPlaces} />
+      </>
+    )
+  }
+
+  if (places.length === 0 && totalPlaces > 0) {
+    return <p className={styles.uasearchresults__notfound}>{t('results.wrongpagination')}</p>
+  }
+
+  return <p className={styles.uasearchresults__notfound}>{t('results.notfound')}</p>
+}
