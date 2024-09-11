aebd202f4c4504ef110209c136827dece143e163
Alessandro Rabitti <axel83.beta@gmail.com>
2024-09-11 11:24:44 +0200

    Improved mongodb options

--- src/helpers/config/mongodb.ts
+++ src/helpers/config/mongodb.ts
@@ -1,3 +1,4 @@
+import type { MongoOptions } from 'mongodb'
 import { MongoClient } from 'mongodb'
 
 import { isEnvironmentDevelopment } from '../utils/isEnvironment'
@@ -17,19 +18,23 @@ function getMongoDbClient(
   let client: MongoClient
   let clientPromise: Promise<MongoClient>
 
+  const options: Partial<MongoOptions> = {
+    retryWrites: true
+  }
+
   if (isEnvironmentDevelopment(environment)) {
     const globalWithMongoClientPromise = global as typeof globalThis & {
       _mongoClientPromise: Promise<MongoClient>
     }
 
     if (!globalWithMongoClientPromise._mongoClientPromise) {
-      client = new MongoClient(uri)
+      client = new MongoClient(uri, options)
       globalWithMongoClientPromise._mongoClientPromise = client.connect()
     }
 
     clientPromise = globalWithMongoClientPromise._mongoClientPromise
   } else {
-    client = new MongoClient(uri)
+    client = new MongoClient(uri, options)
     clientPromise = client.connect()
   }
 
