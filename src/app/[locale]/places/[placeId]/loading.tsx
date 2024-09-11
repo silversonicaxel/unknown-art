fd8f756fff1670518a510e968201da1a3ecc6497
Alessandro Rabitti <axel83.beta@gmail.com>
2024-09-11 14:24:16 +0200

    Fixed translations in loading components

--- src/app/[locale]/places/[placeId]/loading.tsx
+++ src/app/[locale]/places/[placeId]/loading.tsx
@@ -1,6 +1,17 @@
+'use client'
+
+import { useParams } from 'next/navigation'
+
 import { Loading } from 'src/components/loading'
+import { useTranslationClient } from 'src/helpers/hooks/useTranslationClient'
+import { I18nLocale } from 'src/types/i18n'
 
 
 export default function PlacesLoading() {
-  return <Loading text="place" />
+  const params = useParams()
+  const locale = params.locale as I18nLocale
+
+  const { t } = useTranslationClient({ locale, namespace: 'common' })
+
+  return <Loading text={t('menu.place')} />
 }
