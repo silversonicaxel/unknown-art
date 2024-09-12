06f0a0d79c9e2e85dcf9827dc7303da71050cf5f
Alessandro Rabitti <axel83.beta@gmail.com>
2024-09-12 12:47:14 +0200

    Improved e2e tests configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,11 +4,18 @@ import { defineConfig, devices } from '@playwright/test'
 import dotenv from 'dotenv'
 
 
+const PLAYWRIGHT_TEST_TIMEOUT_SECONDS = 120
+const PLAYWRIGHT_EXPECT_TIMEOUT_SECONDS = 10
+const PLAYWRIGHT_RETRIES = 3
+
 dotenv.config({ path: path.resolve(__dirname, '.env') })
 
 const environment = process.env.NEXT_PUBLIC_NODE_ENV || 'development'
 
 export default defineConfig({
+  expect: {
+    timeout: PLAYWRIGHT_EXPECT_TIMEOUT_SECONDS * 1000
+  },
   forbidOnly: environment !== 'development',
   fullyParallel: true,
   projects: [
@@ -30,9 +37,10 @@ export default defineConfig({
     },
   ],
   reporter: 'html',
-  retries: environment !== 'development' ? 2 : 0,
+  retries: environment !== 'development' ? PLAYWRIGHT_RETRIES : 0,
   testDir: './src/',
   testMatch: '**/*.e2e.ts',
+  timeout: PLAYWRIGHT_TEST_TIMEOUT_SECONDS * 1000,
   use: {
     baseURL: process.env.PLAYWRIGHT_BASE_URL || 'http://localhost:3000',
     screenshot: 'only-on-failure',
